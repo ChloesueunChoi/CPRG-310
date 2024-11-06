be46e5488a5bc7cf41ce3686c5eb22104ff72f47
ChloesueunChoi <sueun.choi@edu.sait.ca>
2024-11-05 23:45:15 -0700

    js

--- scripts.js
+++ scripts.js
@@ -1,6 +1,22 @@
 document.getElementById("form").addEventListener("submit", function(event) {
     if (!document.getElementById("agree").checked) {
-        event.preventDefault();
+        event.preventDefault(); 
         alert("You must accept the Terms and Conditions to proceed.");
     }
 });
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    const form = document.getElementById("form");
+    const inputs = document.querySelectorAll("input, select, textarea");
+
+        inputs.forEach(input => {
+            input.addEventListener("focusin", (event) => {
+                event.target.style.backgroundColor = "#EEEEEE"; 
+            });
+        
+            input.addEventListener("focusout", (event) => {
+                event.target.style.backgroundColor = "#DCDCDC";
+            });
+        });
+    });
